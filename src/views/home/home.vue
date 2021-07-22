5061d3a973aa2015b7ba10f782cf3a206e0ad393
coderzym <1505789085@qq.com>
2021-07-22 15:22:24 +0800

    自动格式化

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="contain">
     <div class="head">
-      <img class="default_img"
-           :src="defaultImg" />
-      <el-input v-model="input"
-                placeholder="请输入内容"></el-input>
+      <img class="default_img" :src="defaultImg" />
+      <el-input v-model="input" placeholder="请输入内容"></el-input>
     </div>
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue'
+import { reactive, toRefs } from "vue";
+import { useRoute, useRouter } from "vue-router";
 
 export default {
-  setup () {
+  setup() {
     const state = reactive({
-      defaultImg: require('../../assets/default.jpg'),
-      input: ""
-    })
+      defaultImg: require("../../assets/default.jpg"),
+      input: "",
+    });
+
+    console.log("路由配置", { ...useRoute() });
+    console.log("路由对象", useRouter());
 
     return {
-      ...toRefs(state)
-    }
-  }
+      ...toRefs(state),
+    };
+  },
 };
 </script>
 
